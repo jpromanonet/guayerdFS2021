1241ea427dfd03eaa3ef15eaa53c63ed7a8fb795
jpromanonet <programming@jpromano.net>
2021-12-10 19:26:54 -0300

    Clase 01

--- Clase_01/index.js
+++ Clase_01/index.js
@@ -1,10 +1,14 @@
+// Requerir modulos externos
 var http = require('http');
-var port = 9004
+var dt = require("./moduloFecha");
+
+// Definir el puerto de nuestro servidor Node
+var port = 9009
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('El servidor esta corriendo en el puerto ' + port);
-    res.end('Hola Guayerd!');
+    res.write('El dia y la fecha de hoy es: ' + dt.myDateTime());
+    res.end(); // Es obligatorio que el res.end() este para terminar la conexion.
 }).listen(port);
 
 console.log("Tu servidor esta funcionando en el puerto " + port);
